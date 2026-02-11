8f61e45b66c8e7fccac2097d1a9efc31e82f025e
Hadi Diab <hadiibrahimdiab@gmail.com>
2026-02-12 00:13:04 +0200

    feat: add AdminSidebar component with styling for improved admin navigation

--- src/components/Layout/AdminSidebar.jsx
+++ src/components/Layout/AdminSidebar.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import './AdminSidebar.css';
 
 export default function AdminSidebar() {
   const pathname = usePathname();
